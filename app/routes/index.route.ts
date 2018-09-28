33043ec1129c359fc245b19469bb7a3be395dd12
Gleb Shcherbakov <g.p.shcherbakov@tcsbank.ru>
2018-09-28 10:29:05 +0300

    remove changes

--- app/routes/index.route.ts
+++ app/routes/index.route.ts
@@ -1,21 +1,9 @@
 import { Router, Request, Response } from 'express';
-import * as fetch from 'node-fetch';
 
 const router: Router = Router();
 
-const SSO_URL = 'https://sso-qa2.tcsbank.ru';
-const AUTHORIZE = '/authorize?client_id=cashier';
-
 router.get('/', async (req: Request, res: Response) => {
-    try {
-        const response = await fetch(SSO_URL + AUTHORIZE);
-
-        res.status(200).json(response);
-    } catch (e) {
-        res.status(500).json({
-            "status": "500"
-        });
-    }
+    res.status(200).json({"response": "test"});
 });
 
 export const IndexRoute: Router = router;
